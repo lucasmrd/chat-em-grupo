bde86ad0b6fc4ef19b629862ec87916f8264263f
Lucas Miranda <lucasvnx6@gmail.com>
2024-07-06 18:27:51 -0300

    EventListener de disconnect configurado

--- src/main/java/com/lm/chat/config/WebSocketEventListener.java
+++ src/main/java/com/lm/chat/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.lm.chat.config;
 
+import com.lm.chat.model.Mensagem;
+import com.lm.chat.model.TipoDaMensagem;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,8 +15,20 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations templateDaMensagem;
+
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
 
+        if (username != null) {
+            log.info("Usuario desconectou: {} ", username);
+            var mensagem = Mensagem.builder()
+                    .tipo(TipoDaMensagem.SAIU)
+                    .remetente(username)
+                    .build();
+            templateDaMensagem.convertAndSend("/topic/public", mensagem);
+        }
     }
 }
